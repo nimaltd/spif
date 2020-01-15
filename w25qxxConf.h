db0c9c53439e8140b2ad798a640d21463722a53e
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2020-01-15 09:59:47 +0330

    Update w25qxxConf.h

--- w25qxxConf.h
+++ w25qxxConf.h
@@ -1,10 +1,10 @@
 #ifndef _W25QXXCONFIG_H
 #define _W25QXXCONFIG_H
 
+#define _W25QXX_SPI                   hspi1
+#define _W25QXX_CS_GPIO               FLASH_CS_GPIO_Port
+#define _W25QXX_CS_PIN                FLASH_CS_Pin
+#define _W25QXX_USE_FREERTOS          1
+#define _W25QXX_DEBUG                 0
 
-#define		_W25QXX_SPI										hspi1
-#define		_W25QXX_CS_GPIO								FLASH_CS_GPIO_Port
-#define		_W25QXX_CS_PIN								FLASH_CS_Pin
-#define		_W25QXX_USE_FREERTOS					1
-#define		_W25QXX_DEBUG									0
 #endif
