88e1b99a80af84d4fc1e9ab5ddb369183fab4c7c
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2020-01-15 10:04:30 +0330

    Update w25qxx.h

--- w25qxx.h
+++ w25qxx.h
@@ -1,13 +1,31 @@
 #ifndef _W25QXX_H
 #define _W25QXX_H
 
-#include <stdbool.h>
-#include "spi.h"
+/*
+  Author:     Nima Askari
+  WebSite:    http://www.github.com/NimaLTD
+  Instagram:  http://instagram.com/github.NimaLTD
+  Youtube:    https://www.youtube.com/channel/UCUhY7qY1klJm1d2kulr9ckw
+  
+  Version:    1.1.0
+  
+  
+  Reversion History:
+  
+  (1.1.0)
+  Fix some errors.
+  
+  (1.0.0)
+  First release.
+*/
 
 #ifdef __cplusplus
  extern "C" {
 #endif
-	 
+
+#include <stdbool.h>
+#include "spi.h"
+
 typedef enum
 {
 	W25Q10=1,
@@ -26,22 +44,18 @@ typedef enum
 typedef struct
 {
 	W25QXX_ID_t	ID;
-	uint8_t			UniqID[8];
-
-	uint16_t    PageSize;
-	uint32_t		PageCount;
-	uint32_t		SectorSize;
-	uint32_t		SectorCount;
-	uint32_t		BlockSize;
-	uint32_t		BlockCount;
-
-	uint32_t		CapacityInKiloByte;
-	
-	uint8_t	StatusRegister1;
-	uint8_t	StatusRegister2;
-	uint8_t	StatusRegister3;
-	
-	uint8_t	Lock;
+	uint8_t		UniqID[8];
+	uint16_t	PageSize;
+	uint32_t	PageCount;
+	uint32_t	SectorSize;
+	uint32_t	SectorCount;
+	uint32_t	BlockSize;
+	uint32_t	BlockCount;
+	uint32_t	CapacityInKiloByte;
+	uint8_t		StatusRegister1;
+	uint8_t		StatusRegister2;
+	uint8_t		StatusRegister3;	
+	uint8_t		Lock;
 	
 }w25qxx_t;
 
@@ -49,11 +63,11 @@ extern w25qxx_t	w25qxx;
 //############################################################################
 // in Page,Sector and block read/write functions, can put 0 to read maximum bytes 
 //############################################################################
-bool	W25qxx_Init(void);
+bool		W25qxx_Init(void);
 
-void	W25qxx_EraseChip(void);
-void 	W25qxx_EraseSector(uint32_t SectorAddr);
-void 	W25qxx_EraseBlock(uint32_t BlockAddr);
+void		W25qxx_EraseChip(void);
+void 		W25qxx_EraseSector(uint32_t SectorAddr);
+void 		W25qxx_EraseBlock(uint32_t BlockAddr);
 
 uint32_t	W25qxx_PageToSector(uint32_t	PageAddress);
 uint32_t	W25qxx_PageToBlock(uint32_t	PageAddress);
@@ -61,20 +75,20 @@ uint32_t	W25qxx_SectorToBlock(uint32_t	SectorAddress);
 uint32_t	W25qxx_SectorToPage(uint32_t	SectorAddress);
 uint32_t	W25qxx_BlockToPage(uint32_t	BlockAddress);
 
-bool 	W25qxx_IsEmptyPage(uint32_t Page_Address,uint32_t OffsetInByte,uint32_t NumByteToCheck_up_to_PageSize);
-bool 	W25qxx_IsEmptySector(uint32_t Sector_Address,uint32_t OffsetInByte,uint32_t NumByteToCheck_up_to_SectorSize);
-bool 	W25qxx_IsEmptyBlock(uint32_t Block_Address,uint32_t OffsetInByte,uint32_t NumByteToCheck_up_to_BlockSize);
+bool 		W25qxx_IsEmptyPage(uint32_t Page_Address,uint32_t OffsetInByte,uint32_t NumByteToCheck_up_to_PageSize);
+bool 		W25qxx_IsEmptySector(uint32_t Sector_Address,uint32_t OffsetInByte,uint32_t NumByteToCheck_up_to_SectorSize);
+bool 		W25qxx_IsEmptyBlock(uint32_t Block_Address,uint32_t OffsetInByte,uint32_t NumByteToCheck_up_to_BlockSize);
 
-void 	W25qxx_WriteByte(uint8_t pBuffer,uint32_t Bytes_Address);
-void 	W25qxx_WritePage(uint8_t *pBuffer	,uint32_t Page_Address,uint32_t OffsetInByte,uint32_t NumByteToWrite_up_to_PageSize);
-void 	W25qxx_WriteSector(uint8_t *pBuffer,uint32_t Sector_Address,uint32_t OffsetInByte,uint32_t NumByteToWrite_up_to_SectorSize);
-void 	W25qxx_WriteBlock(uint8_t* pBuffer,uint32_t Block_Address,uint32_t OffsetInByte,uint32_t NumByteToWrite_up_to_BlockSize);
+void 		W25qxx_WriteByte(uint8_t pBuffer,uint32_t Bytes_Address);
+void 		W25qxx_WritePage(uint8_t *pBuffer	,uint32_t Page_Address,uint32_t OffsetInByte,uint32_t NumByteToWrite_up_to_PageSize);
+void 		W25qxx_WriteSector(uint8_t *pBuffer,uint32_t Sector_Address,uint32_t OffsetInByte,uint32_t NumByteToWrite_up_to_SectorSize);
+void 		W25qxx_WriteBlock(uint8_t* pBuffer,uint32_t Block_Address,uint32_t OffsetInByte,uint32_t NumByteToWrite_up_to_BlockSize);
 
-void 	W25qxx_ReadByte(uint8_t *pBuffer,uint32_t Bytes_Address);
-void 	W25qxx_ReadBytes(uint8_t *pBuffer,uint32_t ReadAddr,uint32_t NumByteToRead);
-void 	W25qxx_ReadPage(uint8_t *pBuffer,uint32_t Page_Address,uint32_t OffsetInByte,uint32_t NumByteToRead_up_to_PageSize);
-void 	W25qxx_ReadSector(uint8_t *pBuffer,uint32_t Sector_Address,uint32_t OffsetInByte,uint32_t NumByteToRead_up_to_SectorSize);
-void 	W25qxx_ReadBlock(uint8_t* pBuffer,uint32_t Block_Address,uint32_t OffsetInByte,uint32_t	NumByteToRead_up_to_BlockSize);
+void 		W25qxx_ReadByte(uint8_t *pBuffer,uint32_t Bytes_Address);
+void 		W25qxx_ReadBytes(uint8_t *pBuffer,uint32_t ReadAddr,uint32_t NumByteToRead);
+void 		W25qxx_ReadPage(uint8_t *pBuffer,uint32_t Page_Address,uint32_t OffsetInByte,uint32_t NumByteToRead_up_to_PageSize);
+void 		W25qxx_ReadSector(uint8_t *pBuffer,uint32_t Sector_Address,uint32_t OffsetInByte,uint32_t NumByteToRead_up_to_SectorSize);
+void 		W25qxx_ReadBlock(uint8_t* pBuffer,uint32_t Block_Address,uint32_t OffsetInByte,uint32_t	NumByteToRead_up_to_BlockSize);
 //############################################################################
 #ifdef __cplusplus
 }
