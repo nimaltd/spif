116fb335d1b0079d548a8c6d072b489d15ad6826
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2021-04-04 13:56:37 +0430

    Update w25qxx.c

--- w25qxx.c
+++ w25qxx.c
@@ -142,72 +142,72 @@ bool	W25qxx_Init(void)
 	#if (_W25QXX_DEBUG==1)
 	printf("w25qxx ID:0x%X\r\n",id);
 	#endif
-	switch(id&0x0000FFFF)
+	switch(id&0x000000FF)
 	{
-		case 0x401A:	// 	w25q512
+		case 0x20:	// 	w25q512
 			w25qxx.ID=W25Q512;
 			w25qxx.BlockCount=1024;
 			#if (_W25QXX_DEBUG==1)
 			printf("w25qxx Chip: w25q512\r\n");
 			#endif
 		break;
-		case 0x4019:	// 	w25q256
+		case 0x19:	// 	w25q256
 			w25qxx.ID=W25Q256;
 			w25qxx.BlockCount=512;
 			#if (_W25QXX_DEBUG==1)
 			printf("w25qxx Chip: w25q256\r\n");
 			#endif
 		break;
-		case 0x4018:	// 	w25q128
+		case 0x18:	// 	w25q128
 			w25qxx.ID=W25Q128;
 			w25qxx.BlockCount=256;
 			#if (_W25QXX_DEBUG==1)
 			printf("w25qxx Chip: w25q128\r\n");
 			#endif
 		break;
-		case 0x4017:	//	w25q64
+		case 0x17:	//	w25q64
 			w25qxx.ID=W25Q64;
 			w25qxx.BlockCount=128;
 			#if (_W25QXX_DEBUG==1)
 			printf("w25qxx Chip: w25q64\r\n");
 			#endif
 		break;
-		case 0x4016:	//	w25q32
+		case 0x16:	//	w25q32
 			w25qxx.ID=W25Q32;
 			w25qxx.BlockCount=64;
 			#if (_W25QXX_DEBUG==1)
 			printf("w25qxx Chip: w25q32\r\n");
 			#endif
 		break;
-		case 0x4015:	//	w25q16
+		case 0x15:	//	w25q16
 			w25qxx.ID=W25Q16;
 			w25qxx.BlockCount=32;
 			#if (_W25QXX_DEBUG==1)
 			printf("w25qxx Chip: w25q16\r\n");
 			#endif
 		break;
-		case 0x4014:	//	w25q80
+		case 0x14:	//	w25q80
 			w25qxx.ID=W25Q80;
 			w25qxx.BlockCount=16;
 			#if (_W25QXX_DEBUG==1)
 			printf("w25qxx Chip: w25q80\r\n");
 			#endif
 		break;
-		case 0x4013:	//	w25q40
+		case 0x13:	//	w25q40
 			w25qxx.ID=W25Q40;
 			w25qxx.BlockCount=8;
 			#if (_W25QXX_DEBUG==1)
 			printf("w25qxx Chip: w25q40\r\n");
 			#endif
 		break;
-		case 0x4012:	//	w25q20
+		case 0x12:	//	w25q20
 			w25qxx.ID=W25Q20;
 			w25qxx.BlockCount=4;
 			#if (_W25QXX_DEBUG==1)
 			printf("w25qxx Chip: w25q20\r\n");
 			#endif
 		break;
-		case 0x4011:	//	w25q10
+		case 0x11:	//	w25q10
 			w25qxx.ID=W25Q10;
 			w25qxx.BlockCount=2;
 			#if (_W25QXX_DEBUG==1)
